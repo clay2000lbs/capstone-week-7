8e296bab83bcb879756d0e8ea83fd859ebc9fb16
Clayton  J. Wilkerson <clay2000lbs@gmail.com>
2021-11-29 11:22:11 -0600

    added dir

---
+++ calculator/index.js
@@ -0,0 +1,52 @@
+class Calculator {
+    constructor(previousOperandContainer, currentOperandContainer) {
+        this.previousOperandContainer = previousOperandContainer   
+        this.currentOperandContainer = currentOperandContainer
+        this.clear()
+    }
+
+    clear() {
+        this.currentOperand = ''
+        this.currentOperand = ''
+        this.operation = undefined
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+        this.currentOperandContainer = number
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+        this.currentOperandContainer.innerText = this.currentOperandContainer
+
+    }
+}
+
+const numberBtn = document.querySelectorAll('.number')
+const operandBtn = document.querySelectorAll('.operand')
+const equalsBtn = document.querySelector('#equals')
+const deleteBtn = document.querySelector('#delete')
+const clearBtn = document.querySelector('.clear')
+const previousOperandContainer = document.querySelector('.previous-operand')
+const currentOperandContainer = document.querySelector('.current-operand')
+
+const calculator = new Calculator(previousOperandContainer, currentOperandContainer)
+
+numberBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
